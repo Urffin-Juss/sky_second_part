a378e734a6557b80a75d22b00d383c0a39085f86
Urffin-Juss <mellondefall@gmail.com>
2025-11-05 15:49:28 +0300

    add __

--- src/Category.py
+++ src/Category.py
@@ -9,7 +9,19 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product: Product):
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product")
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+    def get_product(self) -> list[Product]:
+
+        return self.__products
