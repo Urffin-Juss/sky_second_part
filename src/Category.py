7f32db01fcc4410dcbaa3079182999ce032bd17d
Urffin-Juss <mellondefall@gmail.com>
2025-11-07 16:53:49 +0300

    fix tests bag

--- src/Category.py
+++ src/Category.py
@@ -1,7 +1,6 @@
 ﻿from Product import Product
 from src.Base_Product import BaseProduct
 
-
 class Category(BaseProduct):
     category_count = 0
     product_count = 0
@@ -14,15 +13,26 @@ class Category(BaseProduct):
         Category.product_count += len(products)
 
     def add_product(self, product: Product):
+        """Добавляет товар в категорию"""
         if not isinstance(product, Product):
             raise TypeError("В категорию можно добавлять только товары Product")
-
-        # Просто добавляем товар в список
         self.__products.append(product)
 
     @property
     def products(self) -> list[Product]:
         return self.__products
+
+    def average_price(self) -> float:
+        """
+        Подсчитывает средний ценник всех товаров в категории.
+        Если в категории нет товаров, возвращает 0.
+        """
+        try:
+            total_price = sum(product.price for product in self.__products)
+            total_products = len(self.__products)
+            return total_price / total_products
+        except ZeroDivisionError:
+            return 0
 
     def __str__(self) -> str:
         total_quantity = sum(p.quantity for p in self.__products)
@@ -30,17 +40,3 @@ class Category(BaseProduct):
 
     def get_info(self):
         return f"Категория: {self.name}, количество продуктов: {len(self.products)}"
-
-    def average_price(self) -> float:
-
-
-        try:
-
-            total_price = sum(product.price for product in self.__products)
-
-            total_products = len(self.__products)
-
-            return total_price / total_products
-        except ZeroDivisionError:
-
-            return 0
